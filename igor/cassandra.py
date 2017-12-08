208a04b4f93ea1bd5a854a55569ac7534b415bdf
James Liu <jamescliu@hotmail.com>
2017-12-08 11:46:37 +0000

    test

--- igor/cassandra.py
+++ igor/cassandra.py
@@ -1,22 +1,13 @@
-from subprocess import PIPE, Popen
-import json
+import igor.commandline_helper as cmd_hlp
 
 resultset = {}
 script = """
 df -h |awk '/^\//{print $1 " " $8}'
 """
 
-def cmdline(command):
-    process = Popen(
-        args=command,
-        stdout=PIPE,
-        shell=True
-    )
-    return process.communicate()[0]
-
 
 def main(self):
-    raw_output = str(cmdline(script)).splitlines()
+    raw_output = str(cmd_hlp.cmdline(script)).splitlines()
     for line in raw_output:
         l = str(line).split(" ")
         x = l[0]
